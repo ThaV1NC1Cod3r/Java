dadae8ec14eef8f3aacac8c99608aadf8a9651fc
ThaV1NC1Cod3r <ThaV1NC1Cod3r@gmial.com>
2022-07-11 09:27:02 -0400

    Finished CH 1 and started the exercises.

---
+++ src/Ch.1/ShowSyntaxErrors.java
@@ -0,0 +1,6 @@
+public class ShowSyntaxErrors {
+    public static void main(String[] args) {
+        // Will throw syntax error because it is missing the closing set of quotations
+        // System.out.println("Welcome to java); // uncomment and run for the error
+    }
+}
---
+++ src/Ch.1/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("3.5 * 4 / 2 - 2.5 is "); // Prints out text
+        System.out.println(3.5 * 4 / 2 - 2.5); // Prints out calculations
+    }
+}
